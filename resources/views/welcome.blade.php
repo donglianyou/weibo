1f79850e1829ce5156c5702a9330b4f819bd76fc
vagrant <vagrant@homestead>
2017-08-10 05:54:27 +0000

    Say something

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -73,7 +73,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    weibo app
+                    weibo
                 </div>
 
             </div>
